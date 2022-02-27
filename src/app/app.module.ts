5b7d2964a997a87efb5c0c9a118b317b5993a389
Luca <luca8973@hotmail.com>
2022-02-27 15:19:03 +0100

    Completo CV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CvComponent } from './pages/cv/cv.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    CvComponent,
+    HeaderComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
